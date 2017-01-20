92d9086c6d4920b78c47a389c9f87163f75ddee8
Cassie <cassie.pericak@northwestern.edu>
2017-01-20 14:05:52 -0600

    return selected option without text after the number

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,12 +5,15 @@ var friendsData = require("../data/friends.js");
 module.exports = function(app) {
     // Route to api/friends
     app.get("/api/friends", function(req, res) {
+
+        //return the friends data as a json object
         res.json(friendsData);
     });
 
     // Route to create new friend
     app.post("/api/friends", function(req, res) {
 
+        //push the new data to the friends data array
         friendsData.push(req.body);
 
     });
